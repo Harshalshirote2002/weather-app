b30240e1449158801ac82463fafafe1abf268d54
Harshal Shirote <harshalshirote2002@gmail.com>
2023-09-05 23:20:40 +0530

    Fix-fetch over https

--- src/weatherInfo.js
+++ src/weatherInfo.js
@@ -1,6 +1,6 @@
 async function getData(location) {
   const source =
-    `http://api.weatherapi.com/v1/current.json?key=20136a5eccc04689bc640750230509&q=` +
+    `https://api.weatherapi.com/v1/current.json?key=20136a5eccc04689bc640750230509&q=` +
     location;
   const response = await fetch(source, { mode: "cors" });
   const responseData = await response.json();
